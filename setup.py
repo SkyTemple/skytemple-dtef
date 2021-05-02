0a1c21eefc733116b91687ffb80bdc3e85e52514
Parakoopa <parakoopa@live.de>
2021-05-02 16:47:07 +0200

    Version 1.2.1

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 import os
 
 from setuptools import setup, find_packages
@@ -31,7 +31,7 @@ setup(
     long_description_content_type='text/x-rst',
     url='https://github.com/SkyTemple/skytemple-dtef/',
     install_requires=[
-        'skytemple-files >= 1.0.0',
+        'skytemple-files >= 1.2.0',
         'Pillow >= 6.1.0'
     ],
     classifiers=[
