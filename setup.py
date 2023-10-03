d4becc47d16b6d212659f530c4681f2fb2276f2f
Marco Capypara Köpcke <hello@capypara.de>
2023-10-03 11:56:28 +0200

    Python 3.12 Upgrade

--- setup.py
+++ setup.py
@@ -38,9 +38,9 @@ setup(
         'Development Status :: 4 - Beta',
         'Programming Language :: Python',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 )
